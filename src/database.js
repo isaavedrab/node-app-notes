8ca83d8aa81950506286644401a1053a3edf2074
isaav <i.saavedra.dev@outlook.com>
2020-04-11 16:59:57 -0400

    minor changes in database file

--- src/database.js
+++ src/database.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-//mongoose.connect('mongodb://localhost/notes-db-app', {
 mongoose.connect(process.env.MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
