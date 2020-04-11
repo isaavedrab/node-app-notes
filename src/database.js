ed321336693038c8df133766858156eeebebcfcb
isaav <i.saavedra.dev@outlook.com>
2020-04-11 13:25:01 -0400

    change connection string

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/notes-db-app', {
+//mongoose.connect('mongodb://localhost/notes-db-app', {
+mongoose.connect(process.env.MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false        
